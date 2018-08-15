4c77398686886e8c48b1d2a5829c3c5749ffbf9d
KARIYER\ahmet.aygun <faygun89@gmail.com>
2018-08-15 09:24:29 +0300

    style & script refactoring.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,6 +11,7 @@ class SearchBar extends Component{
             <div className='search-bar'>
                 <input 
                     value={this.state.term}
+                    placeholder="Search"
                     onChange={event=> this.onInputChange(event.target.value)}/>
             </div>
         );
